627ac6aa1f0b93a2f4dd7bd8a67bf4e078ae98ea
Diego Gilon <diego.gilon@gmail.com>
2018-04-10 13:55:15 +0200

    adding first charts

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning DB...'
+
+
+Booking.destroy_all
+User.destroy_all
+Route.destroy_all
+
+
+puts 'Creating routes'
+
+route1 = Route.create(from: 'Gothenburg Port, Sweden', to: 'Shanghai Port, China')
+route2 = Route.create(from: 'Antwerp Port, Belgium', to: 'Port Arthur, USA')
+
+
+puts 'Creating Users'
+
+User1 = User.create(shipper:'John Smith', company: 'Max Steel Inc.', email: 'js@imc.com', password: 'lewagon', password_confirmation: 'lewagon')
+User2 = User.create(shipper:'Bob Hans', company: 'Coper Coil Inc.', email: 'bob@imc.com', password: 'lewagon', password_confirmation: 'lewagon')
+User3 = User.create(shipper:'Kate Nelson', company: 'Gen Wheat Inc.', email: 'kate@imc.com', password: 'lewagon', password_confirmation: 'lewagon')
+
+
+puts 'Creating Bookings'
+
+Booking.create(user: User1, route: route1)
+Booking.create(user: User2, route: route1)
+Booking.create(user: User3, route: route1)
+
+
+puts 'Done!'
+
+
+
+
+
