a777158b2c569a8a40e58684ac4ea4d1286ab187
Diego Gilon <diego.gilon@gmail.com>
2019-01-17 18:47:28 +0100

    title changed

--- test/models/route_test.rb
+++ test/models/route_test.rb
@@ -1,7 +1,11 @@
-require 'test_helper'
+require 'rails_helper'
 
-class RouteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+Rspec.describe Route do
+  let(:route) { Route.new(from: 'Antwerp', to: 'Montreal') }
+
+  describe 'Getters' do
+    it 'has an origin port' do
+      expect(route.from).to eq 'Antwerp'
+    end
+  end
 end
